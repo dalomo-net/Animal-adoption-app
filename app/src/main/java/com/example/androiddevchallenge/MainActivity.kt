5cd429225063aeaba36575c94aa7021956288978
PIERCE <etreraviauseptiemeciel@gmail.com>
2021-02-27 22:53:15 +0900

    createList

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,44 +18,184 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import androidx.compose.foundation.Image as Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.ui.Alignment
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyTheme {
-                MyApp()
+                LazyColumn() {
+                    item {
+                        Image(painter = painterResource(id = R.drawable.header), contentDescription = "")
+                    }
+                    item {
+                        Card(R.drawable._00, "Taro")
+                    }
+                    item {
+                        Card(R.drawable._01, "Hanako")
+                    }
+                    item {
+                        Card(R.drawable._02, "Goro")
+                    }
+                    item {
+                        Card(R.drawable._03, "Yasu")
+                    }
+                    item {
+                        Card(R.drawable._04, "Gonzo")
+                    }
+                    item {
+                        Card(R.drawable._05, "Hina")
+                    }
+                    item {
+                        Card(R.drawable._06, "Ken")
+                    }
+                    item {
+                        Card(R.drawable._07, "Kan")
+                    }
+                    item {
+                        Card(R.drawable._08, "Ichi")
+                    }
+                    item {
+                        Card(R.drawable._09, "JF")
+                    }
+                    item {
+                        Card(R.drawable._10, "Dolly")
+                    }
+                }
             }
         }
     }
 }
 
-// Start building your app here!
 @Composable
-fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+fun Card(rid: Int, name: String){
+    val image = painterResource(id = rid)
+
+    Column (
+        Modifier
+//            .clickable(onClick = onClick)
+            .padding(16.dp)
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.End,
+        verticalArrangement = Arrangement.Center
+
+    ) {
+        val imageModifier = Modifier
+            .wrapContentSize()
+        Image(image, "", modifier = imageModifier, contentScale = ContentScale.Crop)
+        Text(text = name, style = TextStyle(fontSize = 20.sp, textAlign = TextAlign.End))
+
     }
 }
+
 
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun LightPreview() {
     MyTheme {
-        MyApp()
+        LazyColumn() {
+            item {
+                Image(painter = painterResource(id = R.drawable.header), contentDescription = "")
+            }
+            item {
+                Card(R.drawable._00, "Taro")
+            }
+            item {
+                Card(R.drawable._01, "Hanako")
+            }
+            item {
+                Card(R.drawable._02, "Goro")
+            }
+            item {
+                Card(R.drawable._03, "Yasu")
+            }
+            item {
+                Card(R.drawable._04, "Gonzo")
+            }
+            item {
+                Card(R.drawable._05, "Hina")
+            }
+            item {
+                Card(R.drawable._06, "Ken")
+            }
+            item {
+                Card(R.drawable._07, "Tomo")
+            }
+            item {
+                Card(R.drawable._08, "Ichi")
+            }
+            item {
+                Card(R.drawable._09, "JF")
+            }
+            item {
+                Card(R.drawable._10, "Dolly")
+            }
+        }
     }
 }
 
 @Preview("Dark Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun DarkPreview() {
-    MyTheme(darkTheme = true) {
-        MyApp()
+    MyTheme {
+        LazyColumn() {
+            item {
+                Image(painter = painterResource(id = R.drawable.header), contentDescription = "")
+            }
+            item {
+                Card(R.drawable._00, "Taro")
+            }
+            item {
+                Card(R.drawable._01, "Hanako")
+            }
+            item {
+                Card(R.drawable._02, "Goro")
+            }
+            item {
+                Card(R.drawable._03, "Yasu")
+            }
+            item {
+                Card(R.drawable._04, "Gonzo")
+            }
+            item {
+                Card(R.drawable._05, "Hina")
+            }
+            item {
+                Card(R.drawable._06, "Ken")
+            }
+            item {
+                Card(R.drawable._07, "Tomo")
+            }
+            item {
+                Card(R.drawable._08, "Ichi")
+            }
+            item {
+                Card(R.drawable._09, "JF")
+            }
+            item {
+                Card(R.drawable._10, "Dolly")
+            }
+        }
     }
 }
